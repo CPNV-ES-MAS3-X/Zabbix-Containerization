fda9172441a234ffd8415ff87d45da46cd4fff05
adieperi <github-a4b3a800f322886f1aa50fc8ea774cd8@aes5m4.de>
2023-12-18 14:03:56 +0100

    Update install-zabbix-agent-debian.sh

--- install-zabbix-agent-debian.sh
+++ install-zabbix-agent-debian.sh
@@ -2,25 +2,43 @@
 
 export PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
-ZABBIX_SERVER='10.0.5.10'
-ZABBIX_AGENT_HOSTNAME='debian'
+echo 'Please enter Zabbix Agent hostname :'
+read ZABBIX_AGENT_HOSTNAME
 
-cat 1>>/etc/sysctl.conf << EOF
-net.ipv6.icmp.echo_ignore_all=1
-net.ipv6.conf.all.disable_ipv6=1
+echo 'Please enter Zabbix Server IP address :'
+read ZABBIX_SERVER
+
+# Debian 11
+# wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4+debian11_all.deb
+
+# Debian 12
+wget https://repo.zabbix.com/zabbix/6.4/debian/pool/main/z/zabbix-release/zabbix-release_6.4-1+debian12_all.deb
+
+# Debian 11
+# dpkg -i zabbix-release_6.0-4+debian11_all.deb
+
+# Debian 12
+dpkg -i zabbix-release_6.4-1+debian12_all.deb
+
+apt-get update
+apt-get install -y sudo zabbix-agent2 zabbix-agent2-plugin-*
+
+cat 1>/etc/zabbix/zabbix_agent2.conf << EOF 
+PidFile=/var/run/zabbix/zabbix_agent2.pid
+LogFile=/var/log/zabbix/zabbix_agent2.log
+LogFileSize=0
+Server=${ZABBIX_SERVER}
+Hostname=${ZABBIX_AGENT_HOSTNAME}
+Include=/etc/zabbix/zabbix_agent2.d/*.conf
+PluginSocket=/run/zabbix/agent.plugin.sock
+ControlSocket=/run/zabbix/agent.sock
+AllowKey=system.run[*]
+DenyKey=system.run[*]
+Include=./zabbix_agent2.d/plugins.d/*.conf
 EOF
 
-sysctl -p
+echo 'zabbix ALL=(ALL) NOPASSWD:/bin/systemctl restart systemd-timesyncd.service' 1>>/etc/sudoers
+echo 'zabbix ALL=(ALL) NOPASSWD:/bin/systemctl restart systemd-timesyncd' 1>>/etc/sudoers
 
-wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4+debian11_all.deb
-dpkg -i zabbix-release_6.0-4+debian11_all.deb
-apt-get update
-apt-get install -y zabbix-agent2 zabbix-agent2-plugin-*
-systemctl enable zabbix-agent2
-sed -i "s/Server=127.0.0.1/Server=${ZABBIX_SERVER}/" /etc/zabbix/zabbix_agent2.conf
-#sed -i "s/ServerActive=127.0.0.1/ServerActive=${ZABBIX_SERVER}/" /etc/zabbix/zabbix_agent2.conf
-sed -i "s/Hostname=Zabbix server/Hostname=${ZABBIX_AGENT_HOSTNAME}/" /etc/zabbix/zabbix_agent2.conf
-sed -i '458 s/^/AllowKey=system.run[*]/' /etc/zabbix/zabbix_agent2.conf
-sed -i '470 s/^/DenyKey=system.run[*]/' /etc/zabbix/zabbix_agent2.conf
-echo 'zabbix ALL=(ALL) NOPASSWD:/bin/systemctl restart apache2' 1>>/etc/sudoers
+systemctl enable zabbix-agent2.service
 systemctl restart zabbix-agent2.service
