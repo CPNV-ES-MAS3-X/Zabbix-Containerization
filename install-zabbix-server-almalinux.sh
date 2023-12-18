606165dea03930f421c67e04d32326dfc5cd3981
adieperi <github-a4b3a800f322886f1aa50fc8ea774cd8@aes5m4.de>
2023-12-18 14:04:26 +0100

    Update install-zabbix-server-almalinux.sh

--- install-zabbix-server-almalinux.sh
+++ install-zabbix-server-almalinux.sh
@@ -3,16 +3,10 @@
 export PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
 echo "max_parallel_downloads=10" 1>>/etc/dnf/dnf.conf
-dnf update -y
+dnf update
 dnf install -y vim epel-release net-tools
 dnf install -y htop
 systemctl disable --now firewalld
-
-cat 1>>/etc/sysctl.conf << EOF
-net.ipv6.conf.all.disable_ipv6=1
-EOF
-
-sysctl -p
 
 rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/8/x86_64/zabbix-release-6.0-4.el8.noarch.rpm
 
@@ -40,8 +34,19 @@ echo 'set global log_bin_trust_function_creators = 0;' | mysql
 sed -i 's/# DBPassword=/DBPassword=password/g' /etc/zabbix/zabbix_server.conf
 sed -i 's/#        listen          8080/        listen          8080/g' /etc/nginx/conf.d/zabbix.conf
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
-#sed -i '38,57 s/^/#/' /etc/nginx/nginx.conf
 
 systemctl enable zabbix-server zabbix-agent nginx php-fpm
+
+cat 1>/etc/logrotate.d/zabbix-server << EOF
+/var/log/zabbix/zabbix_server.log {
+    size 5M
+    rotate 14
+    copytruncate
+    compress
+    missingok
+    notifempty
+    create 0664 zabbix zabbix
+}
+EOF
 
 reboot;exit
